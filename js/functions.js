244478b5739268c2bb01d61d6e78bc876fa8b1cd
Mackenzie <ma.luzarow@gmail.com>
2017-03-22 18:02:20 -0500

    Added truncating of decimals
    Added a function to truncate trailing values of a decimal to a maximum
    of 10 characters,

--- js/functions.js
+++ js/functions.js
@@ -1,57 +1,69 @@
+
+// Truncates trailing numbers behind a decimal to a maximum of 10 numbers
+function truncate (a) {
+    // Check if number is float
+    if (a % 1 != 0) {
+        // Remove this cheap fix !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        return (Number (a.toFixed (10)).toString ());
+    }
+    
+    return (a.toString ());
+}
+
 function add (a, b) {
     a = Number (a.character);
     b = Number (b.character);
     
-    return ((a + b).toString ());
+    return (truncate (a + b));
 }
 
 function subtract (a, b) {
     a = Number (a.character);
     b = Number (b.character);
     
-	return ((b - a).toString ());
+	return (truncate (b - a));
 }
 
 function divide (a, b) {
     a = Number (a.character);
     b = Number (b.character);
     
-	return ((b / a).toString ());
+	return (truncate (b / a));
 }
 
 function multiply (a, b) {
     a = Number (a.character);
     b = Number (b.character);
     
-	return ((a * b).toString ());
+	return (truncate (a * b));
 }
 
 function log (a) {
     a = Number (a.character);
 
-	return (Math.log (a).toString ());
+	return (truncate (Math.log (a)));
 }
 
 function naturalLog (a) {
     a = Number (a.character);
     
-	return (Math.naturalLog (a).toString ());
+	return (truncate (Math.naturalLog (a)));
 }
 
 function sine (a) {
     a = Number (a.character);
     
-    return (Math.sin (a).toString ());
+    return (truncate (Math.sin (a)));
 }
 
 function cosine (a) {
     a = Number (a.character);
     
-    return (Math.cos (a).toString ());
+    return (truncate (Math.cos (a)));
 }
 
 function tangent (a) {
     a = Number (a.character);
     
-    return (Math.tan (a).toString ());
+    return (truncate (Math.tan (a)));
 }
